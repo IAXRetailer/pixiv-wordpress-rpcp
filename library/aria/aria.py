546022dc434063e7b7127b46c0d78a7cd176aa61
H2Sxxa <retailer673@163.com>
2022-05-29 17:35:07 +0800

    fix import

--- library/aria/aria.py
+++ library/aria/aria.py
@@ -1,5 +1,6 @@
+import imp
 from urllib.request import urlopen
-from tool import litelogger
+from library.tool import litelogger
 import json
 def post_to_aria(resourcefolder,date,url,out,customkeylist,customvaulist,rpcport):
     dicta={'refer': url,'dir':resourcefolder+"/images/"+date,"out":out}
