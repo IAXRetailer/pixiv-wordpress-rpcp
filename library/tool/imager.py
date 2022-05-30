8eb0acd0cba295b4797f478bbacfcc79dd76eda9
H2Sxxa <retailer673@163.com>
2022-05-30 14:30:23 +0800

    draft fix

--- library/tool/imager.py
+++ library/tool/imager.py
@@ -10,7 +10,7 @@ def checkimgsize(imgfile,maxsize):
 def imgzman(imgfile,maxsize):
     while checkimgsize(imgfile,maxsize):
         imga=Image.open(imgfile)
-        w,h=imga.sizd
+        w,h=imga.Sized
         imgb=imga.esize((int(w/2),int(h/2)),Image.ANTIALIAS)
         imgb.save(imgfile)
         break
